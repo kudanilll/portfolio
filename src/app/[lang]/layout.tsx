614950aae644d32ff1a311de40910ed7536959ad
kudanilll <achmad24daniel@gmail.com>
2025-03-27 16:45:10 +0700

    improve seo

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -12,8 +12,83 @@ const instrumentSans = Instrument_Sans({
 // Metadata
 export const metadata: Metadata = {
   title: "Portfolio | Achmad Daniel",
-  description: "",
+  description: "Achmad Daniel Syahputra Web Portfolio",
   icons: { icon: "/en/favicon.ico" },
+  openGraph: {
+    title: "Portfolio | Achmad Daniel",
+    description: "Achmad Daniel Syahputra Web Portfolio",
+    url: "https://www.kudaniel.my.id",
+    siteName: "kudaniel",
+    images: [
+      {
+        url: "https://www.kudaniel.my.id/en/assets/images/og.png",
+        alt: "Portfolio | Achmad Daniel",
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "https://www.kudaniel.my.id/en/assets/images/og-alt.png",
+        alt: "Portfolio | Achmad Daniel",
+        width: 1800,
+        height: 1600,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    creatorId: "@achmaddaniel24",
+    title: "Portfolio | Achmad Daniel",
+    description: "Achmad Daniel Syahputra Web Portfolio",
+    images: {
+      url: "https://www.kudaniel.my.id/en/assets/images/og.png",
+      alt: "Portfolio | Achmad Daniel",
+    },
+    creator: "Achmad Daniel Syahputra",
+  },
+  alternates: {
+    canonical: "https://www.kudaniel.my.id",
+    languages: {
+      "en-US": "https://www.kudaniel.my.id/en",
+      "id-ID": "https://www.kudaniel.my.id/id",
+    },
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  keywords: [
+    "kudaniel",
+    "kudanil",
+    "achmad daniel",
+    "achmad daniel syahputra",
+    "achmaddaniel24",
+    "achmaddaniel",
+    "developer",
+    "web developer",
+    "web dev",
+    "mobile developer",
+    "mobile dev",
+    "iot enthusiast",
+    "fullstack developer",
+    "portfolio",
+    "web portfolio",
+  ],
+  publisher: "Vercel",
+  creator: "Achmad Daniel Syahputra",
+  authors: [
+    { name: "Achmad Daniel Syahputra", url: "https://www.kudaniel.my.id" },
+  ],
 };
 
 interface LangParams {
