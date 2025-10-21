cbad3ece2b77b60c8e8ab00a4215cb5470ecc80b
Achmad Daniel Syahputra <achmad24daniel@gmail.com>
2025-10-21 23:06:06 +0700

    refactor(next.config): remove unused images remote patterns configuration

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "aceternity.com",
-        port: "",
-        pathname: "/images/products/thumbnails/new/**",
-      },
-    ],
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
